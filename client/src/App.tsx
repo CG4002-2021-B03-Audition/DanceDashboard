fa05ace18906dcecc2109beda362148ad061f335
kangmingtay <kang.ming1996@gmail.com>
2021-02-10 02:59:45 +0800

    init client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import BrandLogo from './images/BrandLogo.png'
+
+function App() {
+  return (
+    <div>
+      <header>
+        Welcome to dance party!
+      </header>
+      <img src={BrandLogo} alt="Brand Logo"/>
+    </div>
+  );
+}
+
+export default App;
