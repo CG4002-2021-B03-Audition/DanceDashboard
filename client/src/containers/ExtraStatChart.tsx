a626e450147881fbe0d17d8eaf875c32fcc6a273
kangmingtay <kang.ming1996@gmail.com>
2021-03-01 03:00:45 +0800

    add tooltips for extra stats

--- client/src/containers/ExtraStatChart.tsx
+++ client/src/containers/ExtraStatChart.tsx
@@ -1,4 +1,5 @@
-import { CircularProgress, CircularProgressLabel, Stack, Text } from '@chakra-ui/react';
+import { QuestionOutlineIcon } from '@chakra-ui/icons';
+import { CircularProgress, CircularProgressLabel, Stack, Text, Tooltip } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { getProgressColor } from '../utils';
 
@@ -33,6 +34,9 @@ const ExtraStatChart: React.FC<Props> = ({danceData}) => {
             <Stack justify="center" isInline spacing={6}>
                 <CircularProgress value={avgDelay ? avgDelay * 10 : 0} size={64} color={getProgressColor(avgDelay)}>
                     <CircularProgressLabel>
+                        <Tooltip label="Average time difference between the first dancer to perform the move and the last.">
+                            <QuestionOutlineIcon boxSize="0.8em"/>
+                        </Tooltip>
                         <Text fontWeight="bold" fontSize={24}>
                             Avg Delay
                         </Text>
@@ -43,9 +47,12 @@ const ExtraStatChart: React.FC<Props> = ({danceData}) => {
                 </CircularProgress>
                 <CircularProgress value={ avgAccuracy ? avgAccuracy : 0} size={64} color={getProgressColor(avgAccuracy)}>
                     <CircularProgressLabel>
+                        <Tooltip label="Resemblance between your move and the actual.">
+                            <QuestionOutlineIcon boxSize="0.8em"/>
+                        </Tooltip>
                         <Text size="20" fontWeight="bold" fontSize={24} margin="auto" maxWidth="50%" noOfLines={2}>
                             Avg Move Accuracy
-                        </Text>
+                        </Text>                       
                         <Text fontSize={24}>
                             { avgAccuracy ? avgAccuracy.toFixed(1) : 0 }%
                         </Text>
