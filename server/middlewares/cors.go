65a18d69705c4da8efa9d7d7bdf78ad2e01378a7
kangmingtay <kang.ming1996@gmail.com>
2021-04-12 00:47:28 +0800

    allow PATCH request for CORS

--- server/middlewares/cors.go
+++ server/middlewares/cors.go
@@ -9,7 +9,7 @@ func EnableCORS(allowOrigin string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", allowOrigin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, X-CSRF-Token, Token, session, Origin, Host, Connection, Accept-Encoding, Accept-Language, X-Requested-With")
 
 		if c.Request.Method == "OPTIONS" {
